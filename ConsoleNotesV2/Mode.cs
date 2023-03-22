f024c02e5f5b4b1c0aaa0b453691a93b4c09924a
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-21 22:58:06 -0700

    Add Mode enum and finish View mode of app

---
+++ ConsoleNotesV2/Mode.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleNotes;
+
+internal enum Mode
+{
+    /// <summary>
+    /// For when the user is viewing his previously written notes
+    /// Enables the up and down arrow keys for scrolling through the notes
+    /// </summary>
+    ViewNotes,
+    /// <summary>
+    /// For when the user is selecting a note to edit
+    /// </summary>
+    EditNote,
+    /// <summary>
+    /// For when the user is selecting a note to delete
+    /// </summary>
+    DeleteNote,
+    /// <summary>
+    /// For when the user is in the new note menu, but is not writing the note yet
+    /// </summary>
+    NewNote,
+    /// <summary>
+    /// For when the user is in the middle of writing his note
+    /// </summary>
+    WritingNote
+}
--- ConsoleNotesV2/Program.cs
+++ ConsoleNotesV2/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Spectre.Console;
+using System.Data;
+using System.Text.RegularExpressions;
 
 namespace ConsoleNotes;
 
 public class Program
 {
-    public static List<string> Notes = new List<string>();
-    private static Range displayRange = new Range();
+    private static List<string> Notes;
+    private static NotesRange displayRange;
+    private static Mode mode = Mode.View;
 
     public static void Main()
     {
@@ -26,18 +29,27 @@ public class Program
         /* Load notes */
         string notes = File.ReadAllText(notesFilepath);
         Notes = notes.Split("{<sep>}").ToList();
+        if (Notes[Notes.Count - 1].Length == 0)
+            Notes.RemoveAt(Notes.Count - 1);
+
+        /* Show most recent notes */
+        displayRange = new NotesRange((Notes.Count - 11 > 0) ? Notes.Count - 11 : 0, Notes.Count - 1);
 
         /* Wait to continue */
         Console.ReadKey(true);
-        Console.Clear();
+
+        /* Start App */
+        Update();
 
         /* Main loop */
         new KeyEvents(AllKeysEventHandler, new List<ConsoleKey> {
             ConsoleKey.DownArrow,
             ConsoleKey.UpArrow,
+            ConsoleKey.V,
             ConsoleKey.N,
             ConsoleKey.D,
-            ConsoleKey.E
+            ConsoleKey.E,
+            ConsoleKey.Enter,
         }).Start();
     }
 
@@ -45,15 +57,83 @@ public class Program
     {
         ConsoleKey key = keyinfo.Key;
 
-        if (key == ConsoleKey.DownArrow)
+        if (key == ConsoleKey.DownArrow && displayRange.End != Notes.Count - 1 && mode == Mode.ViewNotes)
         {
+            displayRange.Start++;
+            displayRange.End++;
+            Update();
+        } else if (key == ConsoleKey.UpArrow && displayRange.Start != 0 && mode == Mode.ViewNotes)
+        {
+            displayRange.Start--;
+            displayRange.End--;
+            Update();
+        } else if (key == ConsoleKey.V)
+        {
+            // Switch to view mode
+            Console.CursorVisible = false;
+            mode = Mode.ViewNotes;
+            Update();
+        } else if (key == ConsoleKey.N)
+        {
+            // Switch to NewNote mode
+            mode = Mode.NewNote;
+            Update();
+        } else if (key == ConsoleKey.E)
+        {
+            mode = Mode.EditNote;
+            Update();
+        } else if (key == ConsoleKey.D)
+        {
+            mode = Mode.DeleteNote;
+            Update();
+        } else if (key == ConsoleKey.Enter)
+        {
+            if (mode == Mode.WritingNote)
+            {
 
+            }
         }
     };
 
     public static void Update()
     {
+        Console.Clear();
+        Console.SetCursorPosition(0, 0);
 
+        if (mode == Mode.ViewNotes)
+        {
+            // In any given range (s - e), the range starts at s and ends at s + count. count = (e - s + 1)
+            List<string> notes = Notes.GetRange(displayRange.Start, displayRange.End - displayRange.Start + 1);
+
+            for (int i = 0; i < notes.Count; i++)
+            {
+                AnsiConsole.Write(new Panel(new Text(notes[i])).Expand());
+            }
+
+            // Show the new note panel
+            Console.SetCursorPosition(0, 0);
+        } else if (mode == Mode.NewNote)
+        {
+            var rule = new Spectre.Console.Rule("[deeppink3]Create New Note[/]");
+            rule.Style = new Style(Color.Yellow);
+            AnsiConsole.Write(rule);
+
+            var add_title_prompt = new SelectionPrompt<string>()
+                .Title("[yellow]Give your note a [deeppink3]title[/]?[/]")
+                .AddChoices(new[] { "Yes", "No" })
+                .HighlightStyle(new Style(Color.DeepPink3));
+
+            add_title_prompt.DisabledStyle = new Style(Color.Yellow);
+            bool add_title = AnsiConsole.Prompt(add_title_prompt) == "Yes";
+            
+            if (add_title)
+            {
+                string title = AnsiConsole.Ask<string>("[yellow]Title your [deeppink3]note[/]:[/]");
+                Console.WriteLine(title)    ;
+            }
+
+            mode = Mode.WritingNote;
+        }
     }
 
     public static void SaveNotes()
