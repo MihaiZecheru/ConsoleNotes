dc459ddb7e3afe89a4fdc21a4e3f0ba69658c9d9
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-21 22:57:46 -0700

    Add NotesRange class

---
+++ ConsoleNotesV2/NotesRange.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleNotes;
+
+internal struct NotesRange
+{
+    public int Start;
+    public int End;
+
+    public NotesRange(int start, int end)
+    {
+        Start = start;
+        End = end;
+    }
+}
