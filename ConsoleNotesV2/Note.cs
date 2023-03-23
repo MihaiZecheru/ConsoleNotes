c8193c6a6e1927e45013caeaa8222b185f29c5ef
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-23 16:25:01 -0700

    Add Note class for adding titles to notes

---
+++ ConsoleNotesV2/Note.cs
@@ -0,0 +1,23 @@
+﻿using Spectre.Console;
+
+namespace ConsoleNotes;
+
+internal class Note
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+
+    public Note(string title, string body)
+    {
+        Title = title;
+        Body = body;
+    }
+
+    public Spectre.Console.Panel GetAsPanel()
+    {
+        Panel panel = new Panel(new Markup(Body));
+        panel.Header = new PanelHeader(Title);
+        panel.BorderStyle = new Style(ColorCycle.Next());
+        return panel.Expand();
+    }
+}
