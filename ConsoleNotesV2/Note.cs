032cdc078f5d1ce39f194b95e2d061710345cb0e
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-23 22:31:43 -0700

    Add ToString method for allowing the use of Program.Notes.Join to save notes to file

--- ConsoleNotesV2/Note.cs
+++ ConsoleNotesV2/Note.cs
@@ -40,6 +40,11 @@ public class Note
         AnsiConsole.Write(GetAsPanel());
     }
 
+    public override string ToString()
+    {
+        return $"{Title}{TitleSeparator}{Body}";
+    }
+
     public static string ParseLinksMarkup(string text)
     {
         var regex = new Regex(@"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})");
