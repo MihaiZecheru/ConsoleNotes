6ebc78d04874c084ff551dffcc46bd34e48332d3
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-24 02:42:08 -0700

    Removed unused import

--- ConsoleNotesV2/Note.cs
+++ ConsoleNotesV2/Note.cs
@@ -1,11 +1,13 @@
 ﻿using Spectre.Console;
 using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleNotes;
 
 public class Note
 {
     private static Regex LinksRegex = new Regex(@"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})");
+
     public static string NoteSeparator = "{<@SEP>}";
     public static string TitleSeparator = "{<@TITLE>}";
     private static string EmptyTitle = "{{TITLE_EMPTY}}";
