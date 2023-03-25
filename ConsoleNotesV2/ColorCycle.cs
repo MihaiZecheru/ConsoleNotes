dd84d74ab9731a4452922d0bb71d2d8959e66538
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-25 14:19:03 -0700

    Add documentation

--- ConsoleNotesV2/ColorCycle.cs
+++ ConsoleNotesV2/ColorCycle.cs
@@ -8,8 +8,19 @@
 internal static class ColorCycle
 {
     // Capping the cycle at 13 prevents the notes from changing their color as the user scrolls as there are 11 notes displayed at a time
+    /// <summary>
+    /// Amount of colors in the cycle
+    /// </summary>
     private static int ColorsCount = 13;
+
+    /// <summary>
+    /// The current index the cycle is at
+    /// </summary>
     private static int Index = 0;
+
+    /// <summary>
+    /// The list of colors to cycle through
+    /// </summary>
     private static List<Spectre.Console.Color> Cycle = new List<Spectre.Console.Color>()
     {
         Spectre.Console.Color.Maroon,
@@ -36,7 +47,11 @@ internal static class ColorCycle
         Spectre.Console.Color.MediumOrchid3
     };
 
-    public static Spectre.Console.Color Next()
+    /// <summary>
+    /// Cycle the colors once and get the new color
+    /// </summary>
+    /// <returns>The next color in the cycle</returns>
+    public static Spectre.Console.Color CycleNext()
     {
         if (Index == ColorsCount - 1) Index = 0;
         return Cycle[Index++];
