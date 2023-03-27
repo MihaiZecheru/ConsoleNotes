b4dc16377e8ff026d398bfdda282314782591243
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2023-03-26 22:51:32 -0700

    Default settings for the UserSettings class

---
+++ ConsoleNotesV2/DefaultSettings.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+
+namespace ConsoleNotes;
+
+internal static class DefaultSettings
+{
+    public static bool ShowRainbowNotes { get; } = true;
+    public static bool NotesDisplayOrder_OldestFirst { get; } = true;
+    public static bool CreateBackups { get; } = true;
+    public static Color Color1 { get; } = Color.DeepSkyBlue3_1;
+    public static Color Color2 { get; } = Color.Red;
+    public static Color Color3 { get; } = Color.Red;
+    public static Color Color4 { get; } = Color.Red;
+    public static Color Color5 { get; } = Color.Red;
+    public static Color Color6 { get; } = Color.Red;
+    public static Color Color7 { get; } = Color.Red;
+    public static Color Color8 { get; } = Color.Red;
+    public static Color Color9 { get; } = Color.Red;
+    public static Color Color0 { get; } = Color.Red;
+}
