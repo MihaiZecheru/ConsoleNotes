8051fb244013e6c01205cd7b9f1533536d5984e0
Mihai Zecheru <zecheruchris@gmail.com>
2023-08-07 13:27:01 -0700

    Added default colors

--- ConsoleNotesV2/DefaultSettings.cs
+++ ConsoleNotesV2/DefaultSettings.cs
@@ -7,14 +7,14 @@ internal static class DefaultSettings
     public static bool ShowRainbowNotes { get; } = true;
     public static bool NotesDisplayOrder_NewestFirst { get; } = true;
     public static bool DateDayFirst { get; } = false;
-    public static string Color1 { get; } = Color.DeepSkyBlue3_1.ToHex();
-    public static string Color2 { get; } = Color.Red.ToHex();
-    public static string Color3 { get; } = Color.Red.ToHex();
-    public static string Color4 { get; } = Color.Red.ToHex();
-    public static string Color5 { get; } = Color.Red.ToHex();
-    public static string Color6 { get; } = Color.Red.ToHex();
-    public static string Color7 { get; } = Color.Red.ToHex();
-    public static string Color8 { get; } = Color.Red.ToHex();
-    public static string Color9 { get; } = Color.Red.ToHex();
-    public static string Color0 { get; } = Color.Red.ToHex();
+    public static string Color1 { get; } = Color.Tomato.ToHex();
+    public static string Color2 { get; } = Color.Orange.ToHex();
+    public static string Color3 { get; } = Color.Yellow.ToHex();
+    public static string Color4 { get; } = Color.SeaGreen.ToHex();
+    public static string Color5 { get; } = Color.DeepSkyBlue3_1.ToHex();
+    public static string Color6 { get; } = Color.Indigo.ToHex();
+    public static string Color7 { get; } = Color.DeepPink.ToHex();
+    public static string Color8 { get; } = Color.Cyan.ToHex();
+    public static string Color9 { get; } = Color.MediumPurple.ToHex();
+    public static string Color0 { get; } = Color.CornflowerBlue.ToHex();
 }
